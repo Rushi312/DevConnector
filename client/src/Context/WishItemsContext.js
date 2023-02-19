58463926c2cf6112dcf8c26a030a3ee907a5a739
Rushi Bhalodiya <rushipatel42209@gmail.com>
2023-02-19 15:01:58 +0530

    Context Folder Updated

---
+++ client/src/Context/WishItemsContext.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+export const WishItemsContext = createContext({
+    items: [],
+    addItem: () => {},
+    removeItem: () => {},
+    addToCart: () => {}
+})
