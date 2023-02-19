926fdbfc46fab868d95fb4dc86700309a575bf94
Harsh Gondaliya <harsh.gondaliya789@gmail.com>
2023-02-19 12:21:34 +0530

    items

---
+++ server/itemsCollection.js
@@ -0,0 +1,126 @@
+
+
+const itemsCollection = [
+    {
+        "_id": "630b7c690dd4ac34455cb262",
+        "name": "Versace",
+        "category": "men",
+        "color": "black",
+        "type": "shoes",
+        "description": "Odissea chunky leather trainers",
+        "price": 699,
+        "size": [
+            "36",
+            "38",
+            "40"
+        ],
+        "highlights": [
+            "dark-green",
+            "claf-leather",
+            "Medusa-head-motif"
+        ],
+        "image": [
+            {
+                "fieldname": "images",
+                "originalname": "versace-1.jpg",
+                "encoding": "7bit",
+                "mimetype": "image/jpeg",
+                "destination": "./public/men",
+                "filename": "images-1661697129697.jpg",
+                "path": "public\\men\\images-1661697129697.jpg",
+                "size": 48662
+            },
+            {
+                "fieldname": "images",
+                "originalname": "versace-2.jpg",
+                "encoding": "7bit",
+                "mimetype": "image/jpeg",
+                "destination": "./public/men",
+                "filename": "images-1661697129774.jpg",
+                "path": "public\\men\\images-1661697129774.jpg",
+                "size": 36702
+            },
+            {
+                "fieldname": "images",
+                "originalname": "versace-3.jpg",
+                "encoding": "7bit",
+                "mimetype": "image/jpeg",
+                "destination": "./public/men",
+                "filename": "images-1661697129795.jpg",
+                "path": "public\\men\\images-1661697129795.jpg",
+                "size": 34354
+            },
+            {
+                "fieldname": "images",
+                "originalname": "versace-4.jpg",
+                "encoding": "7bit",
+                "mimetype": "image/jpeg",
+                "destination": "./public/men",
+                "filename": "images-1661697129840.jpg",
+                "path": "public\\men\\images-1661697129840.jpg",
+                "size": 48932
+            }
+        ],
+        "createdAt": "2022-08-28T14:32:09.906Z",
+        "updatedAt": "2022-08-28T14:32:09.906Z",
+        "__v": 0
+    },
+   
+    {
+        "_id": "630cccf6d57cdfd0d9b80552",
+        "name": "VEJA Kids",
+        "category": "kids",
+        "color": "#264C0E",
+        "type": "shoes",
+        "description": "Nautico suede sneakers",
+        "price": 96,
+        "size": [
+            "14yrs",
+            " 16yrs"
+        ],
+        "highlights": [
+            "Olive green",
+            " Calf suede",
+            " Logo patch to the side",
+            " Logo patch at the tounge",
+            " Round toe",
+            " Front touch-strap fastening"
+        ],
+        "detail": "Nautico suede sneakers",
+        "image": [
+            {
+                "fieldname": "images",
+                "originalname": "veja-1.jpg",
+                "encoding": "7bit",
+                "mimetype": "image/jpeg",
+                "destination": "./public/kids",
+                "filename": "images-1661783286536.jpg",
+                "path": "public\\kids\\images-1661783286536.jpg",
+                "size": 81799
+            },
+            {
+                "fieldname": "images",
+                "originalname": "veja-2.jpg",
+                "encoding": "7bit",
+                "mimetype": "image/jpeg",
+                "destination": "./public/kids",
+                "filename": "images-1661783286537.jpg",
+                "path": "public\\kids\\images-1661783286537.jpg",
+                "size": 57838
+            },
+            {
+                "fieldname": "images",
+                "originalname": "veja-3.jpg",
+                "encoding": "7bit",
+                "mimetype": "image/jpeg",
+                "destination": "./public/kids",
+                "filename": "images-1661783286538.jpg",
+                "path": "public\\kids\\images-1661783286538.jpg",
+                "size": 111178
+            }
+        ],
+        "createdAt": "2022-08-29T14:28:06.542Z",
+        "updatedAt": "2022-08-29T14:28:06.542Z",
+        "__v": 0
+    }
+]
