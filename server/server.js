031231e6000bfaeb7eb2b0ddb339c0516c94b021
Harsh Gondaliya <harsh.gondaliya789@gmail.com>
2023-02-19 14:06:04 +0530

    final backend

--- server/server.js
+++ server/server.js
@@ -14,5 +14,6 @@ app.use(express.static("public"));
 connectDB()
 
 app.use('/api/items', require("./routes/items"))
+app.use('/api/payment', cors(), require("./routes/payment"))
 
 app.listen(PORT, console.log("Server is running on port ", PORT))
